55807c99cacf6d23811b08a873215e367caa0c36
michael-krontz <michaelkrontz@gmail.com>
2022-03-08 16:46:01 -0500

    wip

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-const menuIcon = <FontAwesomeIcon icon={faBars} />
 
 export default function NavBar() {
   return (
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { slide as Menu } from "react-burger-menu";
 
-export default function Navigation() {
+const Navbar = () => {
+    const [isMenuOpen, handleMenu] = useState(false);
+    const handleCloseMenu = () => {
+      handleMenu(false);
+    };
+    const handleStateChange = (state) => {
+      handleMenu(state.isOpen);
+    };
+
   return (
-    <Menu>
+    <Menu width={ 250 } pageWrapId={"page-wrap"} isOpen={isMenuOpen} onStateChange={handleStateChange}>
         <div className='navigation-logo' style={{ textDecoration: 'none', fontSize: '23px', marginBottom: '20px' }}>BeeFriends</div>
         <div className='bee-line-break'></div>
         <div className="menu-item">
-            <Link to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>About Us</Link>
+            <Link onClick={() => handleCloseMenu()} to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>About Us</Link>
         </div>
 
         <div className="menu-item">
-            <Link to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Conservation</Link>
+            <Link onClick={() => handleCloseMenu()} to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Conservation</Link>
         </div>
 
         <div className="menu-item">
-            <Link to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Blog</Link>
+            <Link onClick={() => handleCloseMenu()} to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Blog</Link>
         </div>
 
         <div className="menu-item">
-            <Link to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Contact Us</Link>
+            <Link onClick={() => handleCloseMenu()} to={'/'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Contact Us</Link>
         </div>
 
         <div className="menu-item">
-            <Link to={'/authcontent'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }}>Apiarist Login</Link>
+            <Link onClick={() => handleCloseMenu()} to={'/authcontent'} style={{ textDecoration: 'none', color: '#FFD15A', fontSize: '18px' }} >Apiarist Login</Link>
         </div>
     </Menu>
+
   )
 }
+
+export default Navbar;
