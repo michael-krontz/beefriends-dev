ff6548d69aa59cb50f03599964b5b99c145f356c
michael-krontz <michaelkrontz@gmail.com>
2022-03-11 09:25:49 -0500

    wip

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
+import Stack from '@mui/material/Stack';
+import Alert from '@mui/material/Alert';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import { Link, useHistory } from 'react-router-dom'
+
+const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#F5B64F',
+        darker: '#E79800',
+      },
+      neutral: {
+        main: '#F9F9F9',
+        contrastText: '#363636',
+      },
+    },
+  });
+
 
 export default function Dashboard() {
     const [error, setError] = useState('')
@@ -21,21 +44,29 @@ export default function Dashboard() {
     }
 
   return (
-    <>
-        <Card>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Profile</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <strong>Email: </strong> {currentUser.email}
-                <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-                Update Profile
-                </Link>
-            </Card.Body>
+    <div className='card-wrapper'>
+        <Card variant="outline">
+            <CardContent>
+                <h2>Profile</h2>
+                
+                <Stack sx={{ width: '95%', margin: 'auto', marginTop: '15px', marginBottom: '8px' }} spacing={2}>
+                {error && <Alert severity="error">{error}</Alert>}
+                </Stack>
+                <Box sx={{ '& > :not(style)': { m: 1, width: '95%' }, }} >
+                    <div className='update-profile-wrapper'>
+                        <div className='email-wrapper'>
+                            <strong>Email: </strong> {currentUser.email}
+                        </div>
+                        <Link to="/update-profile" className="link">
+                        Update Profile
+                        </Link>
+                    </div>
+                    <ThemeProvider theme={theme}>
+                        <Button color="primary" variant="contained" onClick={handleLogout}>Log Out</Button>
+                    </ThemeProvider>
+                </Box>
+            </CardContent>
         </Card>
-        <div className='w-100 text-center mt-2'>
-         <Button variant="link" onClick={handleLogout}>Log Out</Button>
-        </div>
-    </>
-
+    </div>
   )
 }
--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,7 +1,31 @@
 import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
+import Alert from '@mui/material/Alert';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory} from 'react-router-dom'
+
+const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#F5B64F',
+        darker: '#E79800',
+      },
+      neutral: {
+        main: '#64748B',
+        contrastText: '#fff',
+      },
+    },
+  });
+
 
 export default function UpdateProfile() {
     const emailRef = useRef()
@@ -39,31 +63,26 @@ export default function UpdateProfile() {
     }
 
   return (
-      <>
-        <Card>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Update Profile</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit = {handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email}></Form.Control>
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} placeholder='Leave blank to keep the same'></Form.Control>
-                    </Form.Group>
-                    <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmRef} placeholder='Leave blank to keep the same'></Form.Control>
-                    </Form.Group>
-                    <Button disabled={loading} className="w-100 mt-3" type="submit">Update</Button>
-                </Form>
-            </Card.Body>
+      <div className='card-wrapper'>
+        <Card variant="outlined">
+            <CardContent>
+                <h2>Update Profile</h2>
+                <Stack sx={{ width: '95%', margin: 'auto', marginTop: '15px', marginBottom: '8px' }} spacing={2}>
+                    {error && <Alert severity="error">{error}</Alert>}
+                </Stack>
+                
+                <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '95%' }, }} autoComplete="off" onSubmit = {handleSubmit}>
+                    <TextField id="outlined-basic" type="email" label="Email" variant="outlined" inputRef={emailRef} defaultValue={currentUser.email} required/>
+                    <TextField id="outlined-basic" type="password" label="Password" variant="outlined" inputRef={passwordRef} placeholder='Leave blank to keep the same' required/>
+                    <TextField id="outlined-basic" type="password" label="Password Confirmation" variant="outlined" inputRef={passwordConfirmRef} placeholder='Leave blank to keep the same' required/>
+                    <ThemeProvider theme={theme}>
+                        <Button disabled={loading} color="primary" variant="contained" type="submit">Update</Button>
+                    </ThemeProvider>
+                </Box>
+
+            <Link to="/" className='link'>Cancel</Link>
+            </CardContent>
         </Card>
-        <div className='w-100 text-center mt-2'>
-            <Link to="/">Cancel</Link>
-        </div>
-      </>
+      </div>
   )
 }
