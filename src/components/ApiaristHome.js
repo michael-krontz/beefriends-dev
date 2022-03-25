2522a093183d3c1dc6ebaff19a79012f7941d27b
michael-krontz <michaelkrontz@gmail.com>
2022-03-25 06:58:01 -0400

    created apiarist home

--- src/components/ApiaristHome.js
+++ src/components/ApiaristHome.js
@@ -23,60 +23,59 @@ const theme = createTheme({
   });
 
 export default function ApiaristHome() {
+
+
+
   return (
-      <>
+      <div className='apiarist-home-wrapper'> 
         <div className='apiarist-info'>
             <div className='apiarist-header-wrapper'>
-                <h3 className='apiarist-header'>Are you an Apiarist?</h3>
-                <strong>Create a BeeFriends Account!</strong>
-            </div>
-            <p className='p-intro'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie libero sit amet neque iaculis ullamcorper. Curabitur lacinia mauris magna, ac pretium enim lacinia at.</p>
-            
+                <h3 className='apiarist-header'>This is going to be a tagline about connecting apiarists with customers.</h3>
+                <strong>Create a BeeFriends Account! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Something about saving the bees as well. </strong>
+            </div>          
             <div className='line-break'></div>
+
+            <div className='promo-image'>
+                <div className='image'>Promo Image Here</div>
+            </div>
             
             <div className='bullet-wrapper'>
                 <div className='bullet'>
                     <div className='bullet-icon'></div>
-                    <h3 className='bullet-text'>Lorem Ipsum sit amet</h3>
+                    <h3 className='bullet-text'>This is the first reason to sign up.</h3>
                 </div>
 
                 <div className='bullet'>
                     <div className='bullet-icon'></div>
-                    <h3 className='bullet-text'>Lorem Ipsum sit amet</h3>
+                    <h3 className='bullet-text'>This is the second reason to sign up.</h3>
                 </div>
 
                 <div className='bullet'>
                     <div className='bullet-icon'></div>
-                    <h3 className='bullet-text'>Lorem Ipsum sit amet</h3>
-                </div>
-
-                <div className='bullet'>
-                    <div className='bullet-icon'></div>
-                    <h3 className='bullet-text'>Lorem Ipsum sit amet</h3>
+                    <h3 className='bullet-text'>This is the third reason to sign up.</h3>
                 </div>
             </div>
         </div>
 
-      <div className='apiarist-card-wrapper'>
-        <Box sx={{ '& > :not(style)': { m: 1, width: '95%', height: '100%' }, }}>
-                <Card variant='oulined'>
-                    <CardContent>
-                        <div className='cta-text'>
-                        Lorem ipsum dolor sit amet, adipiscint.
-                        </div>
-                        <div className='link-wrapper'>
-                        <ThemeProvider theme={theme}>
-                            <Button component={Link} to={'/authcontent'} color="primary" variant="contained">Create Account</Button>
-                        </ThemeProvider>
-                        </div>
-                        <div className='have-an-account'>
-                            <Link to={'/authcontent'} style={{ textDecoration: 'none', color: '#3e3e3e', fontSize: '14px' }} >Already have an account?</Link>
-                        </div>
-                    </CardContent>
-                </Card>
-            </Box>
+        <div className='apiarist-card-wrapper'>
+            <Box sx={{ '& > :not(style)': { m: 1, width: '95%', height: '100%' }, }}>
+                    <Card variant='oulined'>
+                        <CardContent>
+                            <div className='cta-text'>
+                            Lorem ipsum dolor sit amet, adipiscint.
+                            </div>
+                            <div className='link-wrapper'>
+                            <ThemeProvider theme={theme}>
+                                <Button component={Link} to={'/authcontent'} color="primary" variant="contained">Create Account</Button>
+                            </ThemeProvider>
+                            </div>
+                            <div className='have-an-account'>
+                                <Link to={'/authcontent'} className='link' style={{ fontSize: '14px' }} >Already have an account?</Link>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Box>
+        </div>
       </div>
-
-      </>
   )
 }
